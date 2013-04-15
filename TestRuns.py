2592c319d65c544a490cd6b3d452e554be0b8ada
daron1337 <simone.manini@gmail.com>
2013-04-15 16:24:18 +0200

    changed sleep to 0.1

--- TestRuns.py
+++ TestRuns.py
@@ -256,7 +256,7 @@ class TestRuns(object):
                 while testActions[actionId] == None:
                     outAndErr = app.readboth()
                     for out in outAndErr:
-                        sleep(0.5)
+                        sleep(0.1)
                         for responseId, response in sorted(testCase.responses.iteritems()):
                             if responseId == actionId:
                                 log = response.response
@@ -338,7 +338,7 @@ class TestRuns(object):
                 while testActions[actionId] == None:   
                     outAndErr = app.readboth()
                     for out in outAndErr:
-                        sleep(0.5)
+                        sleep(0.1)
                         for responseId, response in sorted(testCase.responses.iteritems()):
                             if responseId == actionId:
                                 log = response.response
@@ -448,7 +448,7 @@ class TestRuns(object):
                 while testActions[actionId] == None:
                     outAndErr = app.readboth() 
                     for out in outAndErr:
-                        sleep(0.5)
+                        sleep(0.1)
                         for responseId, response in sorted(testCase.responses.iteritems()):
                             if responseId == actionId:
                                 log = response.response
